e6b0a9662561616c6114e75015d38ba8ca3b4405
XN <isxuenan@163.com>
2020-09-20 21:11:54 +0800

    test view

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="app">
+  <span>{{msg}}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'it is a test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
